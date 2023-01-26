2b34e5d9b7c452a93d27b408a01975485306e7df
Yuanyuan Yao <yuanyuan.yao@kuleuven.be>
2023-01-27 00:05:20 +0100

    mask r-cnn +optical flow

--- feutils.py
+++ feutils.py
@@ -2,15 +2,17 @@ import numpy as np
 import cv2 as cv
 import time
 import copy
+import math
 
 
-def HOOF(magnitude, angle, nb_bins, fuzzy=False, normalize=False):
+def HOOF(magnitude, angle, nb_bins, mask=None, fuzzy=False, normalize=False):
 	'''
 	Histogram of (fuzzy) oriented optical flow
 	Inputs:
 	magnitude: magnitude matrix of the optical flow
 	angle: angle matrix of the optical flow
 	nb_bins: number of bins
+	mask: the mask obtained by mask r-cnn
 	fuzzy: whether include fuzzy matrix https://ieeexplore.ieee.org/document/7971947/. Default is False.
 	nomalize: whether normalize the histogram to be a pdf. Default is False such that we don't lose information (sum of the magnitude), 
 	          which gives us more freedom on post-processing (like smoothing).
@@ -18,9 +20,13 @@ def HOOF(magnitude, angle, nb_bins, fuzzy=False, normalize=False):
 	hist: normalized and weighted orientation histogram with size 1 x nb_bins
 	Note: The normalized histogram does not sum to 1; instead, np.sum(hist)*2pi/nb_bins = 1
 	'''
-	# Flatten mag/ang matrices
-	magnitude = magnitude.flatten()
-	angle = angle.flatten()
+	if mask is not None:
+		magnitude = magnitude[mask]
+		angle = angle[mask]
+	else:
+		# Flatten mag/ang matrices
+		magnitude = magnitude.flatten()
+		angle = angle.flatten()
 	# Deal with circular continuity
 	for i in range(len(angle)):
 		while (angle[i] < 0 or angle[i] > 2*np.pi):
@@ -112,7 +118,76 @@ def object_detection_yolo(frame, net, ln, W, H, args, LABELS, detect_label='pers
 	return boxes, confidences, classIDs, idxs, elap
 
 
-def optical_flow_FB(frame, frame_prev, nb_bins=8):
+def object_seg_maskrcnn(frame, net, args, LABELS, detect_label='person'):
+	'''
+	Use pretrained Mask R-CNN network to perform object segmentation
+	Inputs:
+	frame: current frame
+	net: mask r-cnn net for object segmentation
+	args: pre-defined parameters
+	LABELS: labels of all classes
+	detect_label: class to be detected; Default: person
+	Outputs:
+	boxes: boxes of the detected objects
+	confidences: confidences of detected objects 
+	classIDs: indices of the classes objects belong to; use together with LABELS to get the corresponding labels 
+	masks: masks of the object
+	elap: processing time
+	'''
+	start = time.time()
+	# construct a blob from the input frame and then perform a forward
+	# pass of the YOLO object detector, giving us our bounding boxes
+	# and associated probabilities
+	blob = cv.dnn.blobFromImage(frame, swapRB=True, crop=False)
+	net.setInput(blob)
+	(boxes_info, masks_info) = net.forward(["detection_out_final",
+		"detection_masks"])
+	# initialize our lists of detected bounding boxes, confidences,
+	# and masks, respectively
+	boxes = []
+	confidences = []
+	classIDs = []
+	masks = []
+	# loop over the number of detected objects
+	for i in range(0, boxes_info.shape[2]):
+		# extract the class ID of the detection along with the
+		# confidence (i.e., probability) associated with the
+		# prediction
+		classID = int(boxes_info[0, 0, i, 1])
+		confidence = boxes_info[0, 0, i, 2]
+		# filter out weak predictions by ensuring the detected
+		# probability is greater than the minimum probability
+		if confidence > args["confidence"] and LABELS[classID]==detect_label:
+			# scale the bounding box coordinates back relative to the
+			# size of the frame and then compute the width and the
+			# height of the bounding box
+			(H, W) = frame.shape[:2]
+			box = boxes_info[0, 0, i, 3:7] * np.array([W, H, W, H])
+			(startX, startY, endX, endY) = box.astype("int")
+			boxW = endX - startX
+			boxH = endY - startY
+			# extract the pixel-wise segmentation for the object,
+			# resize the mask such that it's the same dimensions of
+			# the bounding box, and then finally threshold to create
+			# a *binary* mask
+			mask = masks_info[i, classID]
+			mask = cv.resize(mask, (boxW, boxH),
+				interpolation=cv.INTER_NEAREST)
+			mask = (mask > args["threshold"])
+			# extract the ROI of the image but *only* extracted the
+			# masked region of the ROI
+			# roi = frame[startY:endY, startX:endX][mask]
+			boxes.append([startX, startY, boxW, boxH])
+			confidences.append(float(confidence))
+			classIDs.append(classID)
+			masks.append(mask)
+
+	end = time.time()
+	elap = end-start
+	return boxes, confidences, classIDs, masks, elap
+
+
+def optical_flow_FB(frame, frame_prev, nb_bins=8, GPU=False):
 	'''
 	Inputs:
 	frame: current frame
@@ -120,7 +195,7 @@ def optical_flow_FB(frame, frame_prev, nb_bins=8):
 	nb_bins: number of bins (for Histogram of Oriented Optical Flow)
 	Outputs:
 	hist: orientation histogram
-	center_xy: NaN (Keep here just to have the same number of outputs as optical_flow_box)
+	box_info: NaN (Keep here just to have the same number of outputs as optical_flow_box)
 	mag: average magnitude (all direction/left/right/up/down) 
 	frame_OF: modified current frame 
 	elap: processing time
@@ -129,7 +204,13 @@ def optical_flow_FB(frame, frame_prev, nb_bins=8):
 	cv.imshow("input", frame)
 	frame_grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 	frame_prev_grey = cv.cvtColor(frame_prev, cv.COLOR_BGR2GRAY)
-	flow = cv.calcOpticalFlowFarneback(frame_prev_grey, frame_grey, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+	if GPU:
+		frame_grey_gpu = cv.cuda_GpuMat(frame_grey)
+		frame_prev_grey_gpu = cv.cuda_GpuMat(frame_prev_grey)
+		optical_flow_gpu = cv.cuda.FarnebackOpticalFlow_create(3, 0.5, False, 15, 3, 5, 1.2, 0)
+		flow = optical_flow_gpu.calc(frame_prev_grey_gpu, frame_grey_gpu, None)
+	else:
+		flow = cv.calcOpticalFlowFarneback(frame_prev_grey, frame_grey, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 	flow_horizontal = flow[..., 0]
 	flow_vertical = flow[..., 1]
 	# Computes the magnitude and angle of the 2D vectors
@@ -147,7 +228,7 @@ def optical_flow_FB(frame, frame_prev, nb_bins=8):
 	])
 	mag = np.asarray(mag)
 	hist = HOOF(magnitude, angle, nb_bins, fuzzy=False, normalize=False)
-	center_xy = np.full((1, 2), np.nan)
+	box_info = np.full((1, 4), np.nan)
 	hsv = np.zeros_like(frame)
 	hsv[..., 0] = angle*180/np.pi/2
 	hsv[..., 1] = 255
@@ -156,10 +237,10 @@ def optical_flow_FB(frame, frame_prev, nb_bins=8):
 	cv.imshow("object detection + optical flow", frame_OF)
 	end = time.time()
 	elap = end - start
-	return hist, center_xy, mag, frame_OF, elap
+	return hist, box_info, mag, frame_OF, elap
 
 
-def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABELS, COLORS, oneobject=True, nb_bins=8):
+def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABELS, COLORS, oneobject=True, nb_bins=8, expand=False):
 	'''
 	Inputs:
 	frame: current frame
@@ -173,7 +254,7 @@ def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABE
 	oneobject: if only select one object with highest confidence
 	Outputs:
 	hist: orientation histogram
-	center_xy: x and y coordinates of the center of the box
+	box_info: x and y coordinates of the center of the box, width and height of the box
 	mag: average magnitude (all direction/left/right/up/down) 
 	frame_OF: modified current frame 
 	elap: processing time
@@ -186,7 +267,7 @@ def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABE
 	if len(idxs)==0:
 		print('WARNING: No object detected! Adding NaN values to features.')
 		hist = np.full((1, nb_bins), np.nan)
-		center_xy = np.full((1, 2), np.nan)
+		box_info = np.full((1, 4), np.nan)
 		mag = np.full((1, 5), np.nan)
 	else:
 		idxs = idxs.flatten()
@@ -198,7 +279,14 @@ def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABE
 			# extract the bounding box coordinates
 			(x, y) = (boxes[i][0], boxes[i][1])
 			(w, h) = (boxes[i][2], boxes[i][3])
-			center_xy = np.expand_dims(np.array([x+w/2, y+h/2]), axis=0)
+			center_x = x + w/2
+			center_y = y + h/2 
+			if expand:
+				w = int(math.sqrt(3)*w)
+				h = int(math.sqrt(3)*h)
+				x = int(center_x-w/2)
+				y = int(center_y-h/2)
+			box_info = np.expand_dims(np.array([center_x, center_y, w, h]), axis=0)
 			ys = max(0,y)
 			yl = min(y+h, frame.shape[0])
 			xs = max(0, x)
@@ -241,4 +329,91 @@ def optical_flow_box(frame, frame_prev, boxes, confidences, classIDs, idxs, LABE
 			cv.imshow("object detection + optical flow", frame_OF)
 	end = time.time()
 	elap = end - start
-	return hist, center_xy, mag, frame_OF, elap
+	return hist, box_info, mag, frame_OF, elap
+
+
+def optical_flow_mask(frame, frame_prev, boxes, confidences, classIDs, masks, LABELS, COLORS, oneobject=True, nb_bins=8, extend=False):
+	'''
+	Inputs:
+	frame: current frame
+	frame_prev: previous frame
+	boxes: boxes of the detected objects
+	confidences: confidences of detected objects 
+	classIDs: indices of the classes objects belong to; use together with LABELS to get the corresponding labels 
+	masks: masks of the object
+	LABELS: all labels
+	COLORS: colors assigned to labels
+	oneobject: if only select one object with highest confidence
+	Outputs:
+	hist: orientation histogram
+	center_xy: x and y coordinates of the center of the box
+	mag: average magnitude (all direction/left/right/up/down) 
+	frame_OF: modified current frame 
+	elap: processing time
+	'''
+	start = time.time()
+	cv.imshow("input", frame)
+	frame_OF = copy.deepcopy(frame)
+	frame_grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	frame_prev_grey = cv.cvtColor(frame_prev, cv.COLOR_BGR2GRAY)
+	if len(boxes)==0:
+		print('WARNING: No object detected! Adding NaN values to features.')
+		hist = np.full((1, nb_bins), np.nan)
+		box_info = np.full((1, 4), np.nan)
+		mag = np.full((1, 5), np.nan)
+	if oneobject:
+		idxs = [np.argmax(np.array(confidences))]
+		# TODO: save features for multiobjects
+		for i in idxs:
+			mask = masks[i]
+			# extract the bounding box coordinates
+			(x, y) = (boxes[i][0], boxes[i][1])
+			(w, h) = (boxes[i][2], boxes[i][3])
+			center_x = x + w/2
+			center_y = y + h/2 
+			box_info = np.expand_dims(np.array([center_x, center_y, w, h]), axis=0)
+			ys = max(0, y)
+			yl = min(y+h, frame.shape[0])
+			xs = max(0, x)
+			xl = min(x+w, frame.shape[1])
+			patch = frame_grey[ys:yl, xs:xl]
+			patch_prev = frame_prev_grey[ys:yl, xs:xl]
+			try:
+				flow_patch = cv.calcOpticalFlowFarneback(patch_prev, patch, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+			except:
+				print('WARNING: empty patches!')
+				break
+			flow_horizontal = flow_patch[..., 0]
+			flow_vertical = flow_patch[..., 1]
+			# Computes the magnitude and angle of the 2D vectors
+			# DON'T TRUST cv.cartToPolar
+			# magnitude, angle = cv.cartToPolar(flow_horizontal, flow_vertical, angleInDegrees=False)
+			magnitude = np.absolute(flow_horizontal+1j*flow_vertical)
+			angle = np.angle(flow_horizontal+1j*flow_vertical)
+			if magnitude.mean() > 1e200:
+				print("ABNORMAL!")
+			mag = []
+			mag.append([
+				magnitude[mask].mean(), # avg magnitude
+				flow_horizontal[np.logical_and(flow_horizontal>=0, mask)].mean(),  # up
+				flow_horizontal[np.logical_and(flow_horizontal<=0, mask)].mean(),  # down
+				flow_vertical[np.logical_and(flow_vertical<=0, mask)].mean(),  # left
+				flow_vertical[np.logical_and(flow_vertical>=0, mask)].mean()  # right
+			])
+			mag = np.asarray(mag)
+			hist = HOOF(magnitude, angle, nb_bins, mask=mask, fuzzy=False, normalize=False)
+			hsv = np.zeros_like(frame[ys:yl, xs:xl, :])
+			hsv[..., 0] = angle*180/np.pi/2
+			hsv[..., 1] = 255
+			hsv[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+			bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+			frame_OF[ys:yl, xs:xl, :] = bgr
+			text = "{}: {:.4f}".format(LABELS[classIDs[i]],	confidences[i])
+			color = [int(c) for c in COLORS[classIDs[i]]]
+			cv.putText(frame_OF, text, (x, y - 5), cv.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+			blended = ((0.2 * np.array(color)) + (0.8 * bgr[mask])).astype("uint8")
+			frame_OF[ys:yl, xs:xl, :][mask] = blended
+			cv.imshow("object detection + optical flow", frame_OF)
+	end = time.time()
+	elap = end - start
+	return hist, box_info, mag, frame_OF, elap
---
+++ maskof.py
@@ -0,0 +1,138 @@
+# Adapted from
+# https://pyimagesearch.com/2018/11/19/mask-r-cnn-with-opencv/
+
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import cv2
+import os
+import feutils
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+	help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+	help="path to output video")
+ap.add_argument("-m", "--mask-rcnn", default='mask-rcnn',
+	help="base path to mask-rcnn directory")
+ap.add_argument('-optic', '--opticalonly', action='store_true',
+    help='Include if use optical only (i.e., without object detection)')
+ap.add_argument('-GPU', '--GPU', action='store_true',
+    help='Include if use GPU acceleration')
+ap.add_argument("-dl", "--detectlabel", type=str, default='person',
+	help="class of objects to be detected")
+ap.add_argument("-nb", "--nbins", type=int, default=8,
+	help="number of bins of the histogram")
+ap.add_argument("-v", "--visualize", type=int, default=0,
+	help="whether or not we are going to visualize each instance")
+ap.add_argument("-c", "--confidence", type=float, default=0.3,
+	help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.2,
+	help="minimum threshold for pixel-wise mask segmentation")
+args = vars(ap.parse_args())
+
+# load the COCO class labels our Mask R-CNN was trained on
+labelsPath = os.path.sep.join([args["mask_rcnn"],
+	"object_detection_classes_coco.txt"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# load the set of colors that will be used when visualizing a given
+# instance segmentation
+colorsPath = os.path.sep.join([args["mask_rcnn"], "colors.txt"])
+COLORS = open(colorsPath).read().strip().split("\n")
+COLORS = [np.array(c.split(",")).astype("int") for c in COLORS]
+COLORS = np.array(COLORS, dtype="uint8")
+
+# derive the paths to the Mask R-CNN weights and model configuration
+weightsPath = os.path.sep.join([args["mask_rcnn"],
+	"frozen_inference_graph.pb"])
+configPath = os.path.sep.join([args["mask_rcnn"],
+	"mask_rcnn_inception_v2_coco_2018_01_28.pbtxt"])
+# load our Mask R-CNN trained on the COCO dataset (90 classes)
+# from disk
+print("[INFO] loading Mask R-CNN from disk...")
+net = cv2.dnn.readNetFromTensorflow(weightsPath, configPath)
+
+if args["GPU"]:
+	net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+video_path = args["input"]
+video_name = video_path.split('/')[-1]
+video_id = video_name.split('_')[0]
+vs = cv2.VideoCapture(video_path)
+writer = None
+(W, H) = (None, None)
+# try to determine the total number of frames in the video file
+try:
+	total = int(vs.get(cv2.CAP_PROP_FRAME_COUNT))
+	print("[INFO] {} total frames in video".format(total))
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+	print("[INFO] could not determine # of frames in video")
+	print("[INFO] no approx. completion time can be provided")
+	total = -1
+
+# First frame
+grabbed, frame_prev = vs.read()
+hist_mtx = np.zeros((1, args["nbins"]))
+box_mtx = np.zeros((1, 4))
+mag_mtx = np.zeros((1, 5))
+
+# loop over frames from the video file stream
+while True:
+	# read the next frame from the file
+	grabbed, frame = vs.read()
+	# if the frame was not grabbed, then we have reached the end
+	# of the stream
+	if not grabbed:
+		break
+	# if the frame dimensions are empty, grab them
+	if W is None or H is None:
+		H, W = frame.shape[:2]
+	if args["opticalonly"]:
+		elap_OS = 0
+		hist, box_info, mag, frame_OF, elap_OF = feutils.optical_flow_FB(frame, frame_prev, nb_bins=8, GPU=args["GPU"])
+	else:
+		# Detect objects 
+		boxes, confidences, classIDs, masks, elap_OS = feutils.object_seg_maskrcnn(frame, net, args, LABELS, detect_label=args["detectlabel"])
+		# Compute the optical flow of the most confidenet detected object
+		hist, box_info, mag, frame_OF, elap_OF = feutils.optical_flow_mask(frame, frame_prev, boxes, confidences, classIDs, masks, LABELS, COLORS, oneobject=True, nb_bins=8)
+	hist_mtx = np.concatenate((hist_mtx, hist), axis=0)
+	box_mtx = np.concatenate((box_mtx, box_info), axis=0)
+	mag_mtx = np.concatenate((mag_mtx, mag), axis=0)
+	frame_prev = frame
+	# check if the video writer is None
+	if writer is None:
+		# initialize our video writer
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(args["output"], fourcc, 30,
+			(frame_OF.shape[1], frame_OF.shape[0]), True)
+		# some information on processing single frame
+		if total > 0:
+			print("[INFO] Object segmentation: single frame took {:.4f} seconds".format(elap_OS))
+			print("[INFO] Optical flow: single frame took {:.4f} seconds".format(elap_OF))
+			print("[INFO] estimated total time to finish: {:.4f}".format((elap_OS+elap_OF) * total))
+	# write the output frame to disk
+	writer.write(frame_OF)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+feats = np.concatenate((hist_mtx, mag_mtx, box_mtx), axis=1)
+print("[INFO] saving features ...")
+np.save('features/histogram.npy', hist_mtx)
+np.save('features/box.npy', box_mtx)
+np.save('features/mag.npy', mag_mtx)
+if args["opticalonly"]:
+	save_path = 'features/' + video_id +'_flow.npy'
+else:
+	save_path = 'features/' + video_id +'_mask.npy'
+np.save(save_path, feats)
+# release the file pointers
+print("[INFO] cleaning up ...")
+writer.release()
+vs.release()
